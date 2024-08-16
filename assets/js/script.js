f2366240bf2b932d5a2143273277a961e417038f
Chima Obutte <obuttechima@rocketmail.com>
2024-08-16 08:05:10 +0000

    Change scores paragraphs to divs, define player and computer score elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,35 @@
-const choices = ["rocke", "paper", "scissors"];
+//Define DOM elements
+const choices = ["rock", "paper", "scissors"];
 const player = document.getElementById("player");
 const computer = document.getElementById("computer");
+const DisplayPlayerScore = document.getElementById("DisplayPlayerScore");
+const DisplayComputerScore = document.getElementById("DisplayComputerScore");
+
+let playerScore = 0;
+let computerScore = 0;
+
+//This function handles the game itself
+function playGame(playerChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+    
+    if(playerChoice === computerChoice) {
+        result = "DRAW!"
+    } else {
+      switch(playerChoice) {
+        case "rock":
+            result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
+            break;
+            case "papper":
+                result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+                case "scissors":
+                  result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!"; 
+                  break;
+      }  
+    }
+    player.textContent = `PLAYER: ${playerChoice}`;
+    computer.textContent = `COMPUTER: ${computerChoice}`;
+    messages.textContent = result;
+}
 
